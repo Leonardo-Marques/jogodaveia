5ec3dd559e66e2d7b17f6702af3cc9f3769cf88c
Leonardo Bitencourt Marques <leonardo.marques@dbserver.com.br>
2023-05-09 01:38:31 -0300

    T1 IA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import { knn } from './knn'
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [IA, setIA] = useState('playing');
+
+
+
+  const handleClick = (i) => {
+    var newSquares = [...squares];
+    if (calculateWinner(squares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+
+    readGameState();
+  };
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {squares[i]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+//<div className="status">{status}</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div className="knn">
+        {IA}
+        </div>
     </div>
   );
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function readGameState() {
+    await sleep(250);
+    const gameState = [];
+    const tableCells = document.querySelectorAll('.square');
+    
+    for (let i = 0; i < tableCells.length; i++) {
+      if (tableCells[i].innerHTML == "X") {
+        gameState.push(1);
+      } else if (tableCells[i].innerHTML == "O") {
+        gameState.push(-1);
+      } else {
+        gameState.push(0);
+      }
+    }
+  
+    const newIA = knn(5,gameState); // Calcula o novo valor de IA
+    setIA(newIA); // Atualiza o estado de IA com o novo valor
+    console.log("Resultado: "+newIA + "Array: "+gameState);
+    return newIA
+  }
 }
+
+function calculateWinner(squares) {
+  const lines = [    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 export default App;
---
+++ src/knn.js
@@ -0,0 +1,35 @@
+const dataset = require('./dataset-treino.json').data;
+
+function calcDist(board1, board2) {
+  let dist = 0;
+  for (let i = 0; i < board1.length; i++) {
+      dist += Math.abs(board1[i] - board2[i]);
+  }
+  return dist;
+}
+
+export function knn(k, input) {
+  if (input.filter(x => x == 0).length > 4) return 'playing'
+  const dists = dataset.map(x => { return {...x, distance: calcDist(input, x.input)}})
+                      .sort((a, b) => a.distance - b.distance)
+                      .slice(0, k)
+  const res = [
+    { class: 'xWins', sum: dists.filter(x => x.output[0] == 1).length },
+    { class: 'oWins', sum: dists.filter(x => x.output[1] == 1).length },
+    { class: 'playing', sum: dists.filter(x => x.output[2] == 1).length },
+  ]
+  return res.sort((a, b) => b.sum - a.sum)[0].class;
+}
+
+function calcAccuracy() {
+  const testDataset = require('./dataset-teste.json').data
+  const acertos = testDataset.filter(x => knn(7, x.input) == getLabel(x.output)).length
+  const accuracy = (acertos / testDataset.length) * 100
+  return accuracy.toFixed(2)
+}
+
+function getLabel(arr) {
+  if (arr[0] == 1) return 'xWins'
+  if (arr[1] == 1) return 'oWins'
+  return 'playing'
+}
